780650dd721cc1cb64366248a06ca1d7849f23f5
1257367428 <1257367428@qq.com>
2018-07-10 14:15:05 +0800

    表格3.0

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ struct student
     char name[20];
     char sex[20];
     int birth;
-    char class1[20];
+    char major[20];
     char telephone[20];
     int judge1,judge2,judge3,judge4,judge5;
     int score;
@@ -28,13 +28,13 @@ int main()
         while(!feof(a))
         {
             fscanf(a,"%d,%[^,],%[^,],%d,%[^,],%[^,],%d,%d,%d,%d,%d,",
-                   &s[i].number,s[i].name,s[i].sex,&s[i].birth,s[i].class1,s[i].telephone,&s[i].judge1,&s[i].judge2,&s[i].judge3,&s[i].judge4,&s[i].judge5);
+                   &s[i].number,s[i].name,s[i].sex,&s[i].birth,s[i].major,s[i].telephone,&s[i].judge1,&s[i].judge2,&s[i].judge3,&s[i].judge4,&s[i].judge5);
             i++;
         }
         j=i;
         for(i=0;i<j;i++)
         {
-            printf("%d,%s,%s,%d,%s,%s,%d,%d,%d,%d,%d\n",s[i].number,s[i].name,s[i].sex,s[i].birth,s[i].class1,s[i].telephone,s[i].judge1,s[i].judge2,s[i].judge3,s[i].judge4,s[i].judge5);
+            printf("%d,%s,%s,%d,%s,%s,%d,%d,%d,%d,%d\n",s[i].number,s[i].name,s[i].sex,s[i].birth,s[i].major,s[i].telephone,s[i].judge1,s[i].judge2,s[i].judge3,s[i].judge4,s[i].judge5);
         }
         fclose(a);
     }
@@ -47,26 +47,19 @@ int main()
     j=i;
     for(i=0;i<j;i++)
     {
-        if(s[i].judge2>max[i])
-            max[i]=s[i].judge2;
-        if(s[i].judge3>max[i])
-            max[i]=s[i].judge3;
-        if(s[i].judge4>max[i])
-            max[i]=s[i].judge4;
-        if(s[i].judge5>max[i])
-            max[i]=s[i].judge5;
+        if(s[i].judge2>max[i]) max[i]=s[i].judge2;
+        if(s[i].judge3>max[i]) max[i]=s[i].judge3;
+        if(s[i].judge4>max[i]) max[i]=s[i].judge4;
+        if(s[i].judge5>max[i]) max[i]=s[i].judge5;
+
     }
     j=i;
     for(i=0;i<j;i++)
     {
-        if(s[i].judge2<min[i])
-            min[i]=s[i].judge2;
-        if(s[i].judge3<min[i])
-            min[i]=s[i].judge3;
-        if(s[i].judge4<min[i])
-            min[i]=s[i].judge4;
-        if(s[i].judge5<min[i])
-            min[i]=s[i].judge5;
+        if(s[i].judge2<min[i]) min[i]=s[i].judge2;
+        if(s[i].judge3<min[i]) min[i]=s[i].judge3;
+        if(s[i].judge4<min[i]) min[i]=s[i].judge4;
+        if(s[i].judge5<min[i]) min[i]=s[i].judge5;
     }
     j=i;
     for(i=0;i<j;i++)
@@ -75,13 +68,13 @@ int main()
     }
     
     b=fopen("/Users/S20171105117/Desktop/studentdada2.0.csv","w");
-              fprintf(b,"number,name,sex,birth,class,telephone,judge1,judge2,judge3,judge4,judge5,score\n");
+              fprintf(b,"number,name,sex,birth,major,telephone,judge1,judge2,judge3,judge4,judge5,score\n");
               j=i;
               i=0;
               while(i<j)
               {
                   fprintf(b,"%d,%s,%s,%d,%s,%s,%d,%d,%d,%d,%d,%d\n",s[i].number,s[i].name,s[i].sex,s[i].birth,
-                          s[i].class1,s[i].telephone,s[i].judge1,s[i].judge2,s[i].judge3,s[i].judge4,s[i].judge5,s[i].score);
+                          s[i].major,s[i].telephone,s[i].judge1,s[i].judge2,s[i].judge3,s[i].judge4,s[i].judge5,s[i].score);
                   i++;
               }
               fclose(b);             
